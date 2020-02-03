4836ea0c389060715890dbf698d508e94f5660df
Ivan Yerkinov <darkblackvoid@yahoo.com>
2020-02-02 19:52:05 -0800

    Users can submit and save posts

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const posts = require('./controllers/posts.js')(app);
+
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+const mongod = require('./data/reddit-db');
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
 //Middleware
 const exphbs  = require('express-handlebars');
 
@@ -11,5 +25,10 @@ app.set('view engine', 'handlebars');
 //Routes
 
 app.get('/', (req, res) => res.render('home'))
+
+app.get('/posts/new', (req, res) => res.render('posts-new'))
+
+
+//Listen
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
