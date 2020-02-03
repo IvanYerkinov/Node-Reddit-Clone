74b4389ddb6c007b0bf18c09029e1772a00e5834
Ivan Yerkinov <darkblackvoid@yahoo.com>
2020-02-02 19:38:15 -0800

    hello world peoject init

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//Middleware
+const exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//Routes
+
+app.get('/', (req, res) => res.render('home'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
