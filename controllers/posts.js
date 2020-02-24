140490950dd8fb13c171707454abbf75e1e7017e
Ivan Yerkinov <darkblackvoid@yahoo.com>
2020-02-24 13:43:12 -0800

    Users must be logged in to submit a post

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,18 +3,30 @@ const Post = require('../models/post');
 module.exports = (app) => {
 
   // CREATE
-  app.post('/posts/new', (req, res) => {
-    // INSTANTIATE INSTANCE OF POST MODEL
-    const post = new Post(req.body);
+  // CREATE
+ app.post("/posts/new", (req, res) => {
+   if (req.user) {
+     var post = new Post(req.body);
 
-    // SAVE INSTANCE OF POST MODEL TO DB
-    post.save((err, post) => {
-      console.log(err);
-      console.log(post);
-      return res.redirect(`/`);
+     post.save(function(err, post) {
+       return res.redirect(`/`);
+     });
+   } else {
+     return res.status(401); // UNAUTHORIZED
+   }
+ });
+
+  app.get("/", (req, res) => {
+  var currentUser = req.user;
+
+  Post.find({})
+    .then(posts => {
+      res.render("posts-index", { posts, currentUser });
     })
-
-  });
+    .catch(err => {
+      console.log(err.message);
+    });
+});
 
   app.get('/posts/new', (req, res) => res.render("posts-new"));
 
